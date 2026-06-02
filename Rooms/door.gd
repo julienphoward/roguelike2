6859754380ef08d19232bb4f9900d0f702be719d
Julien <julienphoward@gmail.com>
2026-06-02 12:34:34 -0700

    room1 complete

--- Rooms/door.gd
+++ Rooms/door.gd
@@ -2,5 +2,5 @@ extends StaticBody2D
 @onready var animation_player: AnimationPlayer = get_node("AnimationPlayer")
 
 func open() -> void:
-	animation_player.play("open")
+	animation_player.play("Open")
 	
